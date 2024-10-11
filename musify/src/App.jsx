ed62534ffcd9dc5d9af97ea4df9b9827f91f9714
Kummirai <ajaxmilton@hotmail.com>
2024-10-11 22:40:03 +0200

    Refactor code structure and add routing to Home page

--- musify/src/App.jsx
+++ musify/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <>Hewllol</>;
+  return (
+    <>
+    <Home />
+    </>
+  )
 }
 
 export default App;
--- musify/src/pages/Home.jsx
+++ musify/src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Library from "./Library";
+import Feed from "./Feed";
+import Favorites from "./Favorites";
+import Player from "./Player";
+import Trending from "./Trending";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Library />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/player" element={<Player />} />
+          <Route path="/trending" element={<Trending />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default Home;
