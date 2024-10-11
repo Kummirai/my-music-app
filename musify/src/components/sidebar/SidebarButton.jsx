fb5bc88738cb844f5623b5eec049dea5ed745d9d
Kummirai <ajaxmilton@hotmail.com>
2024-10-12 00:24:55 +0200

    Add more styles to side bar buttons

--- musify/src/components/sidebar/SidebarButton.jsx
+++ musify/src/components/sidebar/SidebarButton.jsx
@@ -1,13 +1,19 @@
 import "./sideBarButton.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 import { IconContext } from "react-icons/lib";
 
 function SidebarButton(props) {
+  const location = useLocation();
+
+  const isActive = location.pathname === props.to;
+
+  const btnClass = isActive ? "btn-container active" : "btn-container";
+
   return (
     <Link to={props.to}>
-      <div className="btn-container">
-        <IconContext.Provider value={{ size: '24px', className:"btn-icon" }}>
+      <div className={btnClass}>
+        <IconContext.Provider value={{ size: "24px", className: "btn-icon" }}>
           {props.icon}
           <p className="title">{props.title}</p>
         </IconContext.Provider>
