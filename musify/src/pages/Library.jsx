2438662e997372da91dc8bbfb283b32c36a99049
Kummirai <ajaxmilton@hotmail.com>
2024-10-12 11:16:33 +0200

    Refactor Library component: add Spotify API call and display playlists
    - Import APIKit and useState in Library component
    - Add useEffect hook to fetch user playlists from Spotify API
    - Display user playlists in the Library component

--- musify/src/pages/Library.jsx
+++ musify/src/pages/Library.jsx
@@ -1,5 +1,28 @@
+import APIKit from "../spotify";
+import { useState, useEffect } from "react";
+import "./library.css";
+
 function Library() {
-  return <div className="page-container">Library</div>;
+  const [playlists, setPlaylists] = useState(null);
+
+  useEffect(() => {
+    APIKit.get("me/playlists").then((response) => {
+      setPlaylists(response.data.items);
+      console.log(response.data.items);
+    });
+  }, []);
+
+  return (
+    <div className="page-container">
+      <div className="library-container">
+        {playlists?.map((playlist) => (
+          <div key={playlist.id}>
+            <div>{playlist.name}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Library;
