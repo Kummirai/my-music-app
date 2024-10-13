c8474ace710a61761d57e7ac95614acf2a494fad
Kummirai <ajaxmilton@hotmail.com>
2024-10-13 19:33:37 +0200

    Refactor Queue component styles and add "Up Next" section

--- musify/src/components/queue/Queue.jsx
+++ musify/src/components/queue/Queue.jsx
@@ -1,11 +1,37 @@
 import "./Queue.css";
+import PropTypes from "prop-types";
 
 function Queue({ tracks, setCurrentTrackIndex }) {
-  return <div className="queue-container">
-    <div className="queue">
-      
+  return (
+    <div className="queue-container">
+      <div className="queue">
+        <p className="upNext">Up Next</p>
+        <div className="queue-list">
+          {tracks.map((track, index) => (
+            <div key={index}>
+              <p>{track.track.name}</p>
+              <p>{track.track.artists[0].name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 }
+Queue.propTypes = {
+  tracks: PropTypes.arrayOf(
+    PropTypes.shape({
+      track: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        artists: PropTypes.arrayOf(
+          PropTypes.shape({
+            name: PropTypes.string.isRequired,
+          })
+        ).isRequired,
+      }).isRequired,
+    })
+  ).isRequired,
+  setCurrentTrackIndex: PropTypes.func.isRequired,
+};
 
 export default Queue;
