482ceb64a7ecc9d8047b8b108425407f90745eb7
Kummirai <ajaxmilton@hotmail.com>
2024-10-13 22:53:21 +0200

    Refactor audio player component: Update ProgressCircle color and percentage

--- musify/src/components/audioPlayer/ProgressCircle.jsx
+++ musify/src/components/audioPlayer/ProgressCircle.jsx
@@ -1,42 +1,70 @@
-import "./ProgressCircle.css";
+import "./progressCircle.css";
 import PropTypes from "prop-types";
 
 const Circle = ({ color, percentage, size, strokeWidth }) => {
   const radius = size / 2 - 10;
-  const circumference = 2 * Math.PI * radius - 20;
-  const strokePercent = ((100 - Math.round(percentage)) * circumference) / 100;
+  const circ = 2 * Math.PI * radius - 20;
+  const strokePct = ((100 - Math.round(percentage)) * circ) / 100;
+
   return (
     <circle
       r={radius}
       cx="50%"
       cy="50%"
       fill="transparent"
-      stroke={strokePercent !== circumference ? color : ""}
-      strokeDasharray={circumference}
-      strokeDashoffset={percentage ? strokePercent : 0}
+      stroke={strokePct !== circ ? color : ""}
+      strokeWidth={strokeWidth}
+      strokeDasharray={circ}
+      strokeDashoffset={percentage ? strokePct : 0}
       strokeLinecap="round"
-    />
+    ></circle>
   );
 };
 
-//props validation
-Circle.propTypes = {
-  color: PropTypes.string,
-  percentage: PropTypes.number,
-  size: PropTypes.number,
-  strokeWidth: PropTypes.string,
-};
-
-function ProgressCircle({ percentage, isPlaying, image, size, color }) {
+export default function ProgressCircle({
+  percentage,
+  isPlaying,
+  size,
+  color,
+  image,
+}) {
   return (
-    <div className="progress-circle">
+    <div className="progress-circle flex">
       <svg width={size} height={size}>
-        <Circle color={"white"} size={size} strokeWidth={"0.4rem"} />
-        <Circle
-          color={color}
-          percentage={percentage}
-          size={size}
-          strokeWidth={"0.6rem"}
+        <g>
+          <Circle strokeWidth={"0.4rem"} color="#3B4F73" size={size} />
+          <Circle
+            strokeWidth={"0.6rem"}
+            color={color}
+            percentage={percentage}
+            size={size}
+          />
+        </g>
+        <defs>
+          <clipPath id="myCircle">
+            <circle cx="50%" cy="50%" r={size / 2 - 30} fill="#FFFFFF" />
+          </clipPath>
+          <clipPath id="myInnerCircle">
+            <circle cx="50%" cy="50%" r={size / 2 - 100} fill="#FFFFFF" />
+          </clipPath>
+        </defs>
+        <image
+          className={isPlaying ? "active" : ""}
+          x={30}
+          y={30}
+          width={2 * (size / 2 - 30)}
+          height={2 * (size / 2 - 30)}
+          href="https://pngimg.com/uploads/vinyl/vinyl_PNG107.png"
+          clipPath="url(#myCircle)"
+        />
+        <image
+          className={isPlaying ? "active" : ""}
+          x={100}
+          y={100}
+          width={2 * (size / 2 - 100)}
+          height={2 * (size / 2 - 100)}
+          href={image}
+          clipPath="url(#myInnerCircle)"
         />
       </svg>
     </div>
@@ -47,9 +75,7 @@ function ProgressCircle({ percentage, isPlaying, image, size, color }) {
 ProgressCircle.propTypes = {
   percentage: PropTypes.number,
   isPlaying: PropTypes.bool,
-  image: PropTypes.string,
   size: PropTypes.number,
   color: PropTypes.string,
+  image: PropTypes.string,
 };
-
-export default ProgressCircle;
