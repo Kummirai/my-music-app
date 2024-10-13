847867e47409ef3a5aa69f0d425b85fd2c89fecd
Kummirai <ajaxmilton@hotmail.com>
2024-10-13 18:50:49 +0200

    Refactor AlbumInfo component to display album information and artist names

--- musify/src/components/songCard/AlbumInfo.jsx
+++ musify/src/components/songCard/AlbumInfo.jsx
@@ -1,18 +1,24 @@
 import PropTypes from "prop-types";
+import "./albumInfo.css";
 
 function AlbumInfo({ album }) {
   console.log(album);
 
+  const artist = [];
+  album?.artists.forEach((artist) => {
+    artist.push(artist.name);
+  });
+
   return (
-    <div>
+    <div className="albumInfo-card">
       <div className="albumName-container">
-        <p></p>
+        <p>{album?.name + "-"+ artist?.join(",")}</p>
       </div>
       <div className="album-info">
-        <p></p>
+        <p>{`${album?.name} is an ${album?.album_type} by ${artist?.join(",")} with ${album?.total_tracks} tracks `}</p>
       </div>
       <div className="album-release">
-        <p></p>
+        <p>Release Date: {album?.release_date}</p>
       </div>
     </div>
   );
