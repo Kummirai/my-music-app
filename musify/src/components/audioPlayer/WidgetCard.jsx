fe32d62f68f8d176a067d5044ba2d64e4a3269d2
Kummirai <ajaxmilton@hotmail.com>
2024-10-14 15:49:18 +0200

    Refactor WidgetCard component: Add props validation and improve rendering logic

--- musify/src/components/audioPlayer/WidgetCard.jsx
+++ musify/src/components/audioPlayer/WidgetCard.jsx
@@ -1,7 +1,48 @@
 import "./widgetCard.css";
+import PropTypes from "prop-types";
 
-function WidgetCard() {
-  return <div>WidgetCard</div>;
+function WidgetCard({ title, similar, featured, newRelease }) {
+  return (
+    <div className="widgetcard-body">
+      <p className="widget-title">{title}</p>
+      {similar
+        ? similar.map((artist) => (
+            <WidgetEntry
+              key={artist.id} // Add a unique key
+              title={artist.name}
+              subtitle={`${artist.followers.total} followers`}
+              image={artist.images[2].url}
+            />
+          ))
+        : featured
+        ? featured.map((playlist) => (
+            <WidgetEntry
+              key={playlist.id} // Add a unique key
+              title={playlist.name}
+              subtitle={playlist.description}
+              image={playlist.images[2].url}
+            />
+          ))
+        : newRelease
+        ? newRelease.map((album) => (
+            <WidgetEntry
+              key={album.id} // Add a unique key
+              title={album.name}
+              subtitle={album.artists[0].name}
+              image={album.images[2].url}
+            />
+          ))
+        : null}
+    </div>
+  );
 }
+
+// Add props validation
+WidgetCard.propTypes = {
+  title: PropTypes.string,
+  similar: PropTypes.array,
+  featured: PropTypes.array,
+  newRelease: PropTypes.array,
+};
 
 export default WidgetCard;
