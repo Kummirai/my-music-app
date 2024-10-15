74e8220858558d0f3f1a53594ee79c7154ea16f7
Kummirai <ajaxmilton@hotmail.com>
2024-10-15 19:39:09 +0200

    Refactor WidgetCard component: Adjust layout for responsiveness and add title element

--- musify/src/components/audioPlayer/WidgetCard.jsx
+++ musify/src/components/audioPlayer/WidgetCard.jsx
@@ -5,37 +5,37 @@ import WidgetEntry from "./WidgetEntry";
 function WidgetCard({ title, similar, featured, newRelease }) {
   return (
     <div className="widgetcard-header">
-    <p className="widget-title">{title}</p>
-    <div className="widgetcard-body">
-      {similar
-        ? similar.map((artist) => (
-            <WidgetEntry
-              key={artist.id} // Add a unique key
-              title={artist?.name}
-              subtitle={`${artist?.followers?.total} followers`}
-              image={artist?.images[2]?.url}
-            />
-          ))
-        : featured
-        ? featured.map((playlist) => (
-            <WidgetEntry
-              key={playlist.id} // Add a unique key
-              title={playlist?.name}
-              subtitle={playlist?.tracks?.total + " songs"}
-              image={playlist?.images[0]?.url}
-            />
-          ))
-        : newRelease
-        ? newRelease.map((album) => (
-            <WidgetEntry
-              key={album.id} // Add a unique key
-              title={album?.name}
-              subtitle={album?.artists[0]?.name}
-              image={album?.images[2]?.url}
-            />
-          ))
-        : null}
-    </div>
+      <p className="widget-title">{title}</p>
+      <div className="widgetcard-body">
+        {similar
+          ? similar.map((artist) => (
+              <WidgetEntry
+                key={artist.id} // Add a unique key
+                title={artist?.name}
+                subtitle={`${artist?.followers?.total} followers`}
+                image={artist?.images[2]?.url}
+              />
+            ))
+          : featured
+          ? featured.map((playlist) => (
+              <WidgetEntry
+                key={playlist.id} // Add a unique key
+                title={playlist?.name}
+                subtitle={playlist?.tracks?.total + " songs"}
+                image={playlist?.images[0]?.url}
+              />
+            ))
+          : newRelease
+          ? newRelease.map((album) => (
+              <WidgetEntry
+                key={album.id} // Add a unique key
+                title={album?.name}
+                subtitle={album?.artists[0]?.name}
+                image={album?.images[2]?.url}
+              />
+            ))
+          : null}
+      </div>
     </div>
   );
 }
